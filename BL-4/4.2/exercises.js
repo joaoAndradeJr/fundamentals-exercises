0339672f2f789568148198c9630356ad154e745f
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-14 13:59:56 -0400

    add: exercises 4, 5, 6 and 7

--- BL-4/4.2/exercises.js
+++ BL-4/4.2/exercises.js
@@ -1,25 +1,85 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // 1
-for (let i = 0; i < numbers.length; i += 1) {
-  console.log(numbers[i]);
+function print() {
+  for (let i = 0; i < numbers.length; i += 1) {
+    console.log(numbers[i]);
+  }
 }
 
 // 2
-let sum = 0;
-for (let i = 0; i < numbers.length; i += 1) {
-  sum += numbers[i];
+function sum() {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i += 1) {
+    sum += numbers[i];
+  }
+  
+  console.log(sum);
 }
-
-console.log(sum);
 
 // 3
-sum = 0;
-let media = undefined;
-
-for (let i = 0; i < numbers.length; i += 1) {
-  sum += numbers[i];
+function media() {
+  let sum = 0;
+  let media = undefined;
+  
+  for (let i = 0; i < numbers.length; i += 1) {
+    sum += numbers[i];
+  }
+  
+  media = sum / numbers.length;
+  console.log(media);
 }
 
-media = sum / numbers.length;
-console.log(media);
+// 4
+function bigger_then_20() {
+  result = media();
+
+  if (result > 20) {
+    console.log('Valor maior que 20');
+  } else {
+    console.log('Valor menor ou igual a 20');
+  }
+}
+
+// 5
+function bigger_number() {
+  actual = numbers[0];
+
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] > actual) {
+      actual = numbers[i];
+    }
+  }
+
+  console.log(actual);
+}
+
+// 6
+function odd_numbers() {
+  let total = 0;
+
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] % 2 != 0) {
+      total += 1;
+    }
+  }
+
+  if (total > 0) {
+    console.log(total);
+  } else {
+    console.log('Nenhum valor ímpar encontrado');
+  }
+}
+
+// 7
+function lower_number() {
+  actual = numbers[0];
+
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] < actual) {
+      actual = numbers[i];
+    }
+  }
+
+  console.log(actual);
+}
