2ba8109103d96fb81f7d20ec29cf0d584fa7db79
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-14 19:57:36 -0400

    add: ex 3

--- BL-4/4.4/exercises2.js
+++ BL-4/4.4/exercises2.js
@@ -13,21 +13,34 @@ function is_palindrome(word) {
 
 console.log(is_palindrome('arara'));
 
-// Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+// 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
 
 function find_bigger(numbers) {
   bigger = numbers[0];
-  position = undefined;
 
   for (number in numbers) {
     if (numbers[number] > bigger) {
-      bigger = number;
+      bigger = numbers[number];
     }
   }
 
-  position = numbers.indexOf(bigger);
-
-  return bigger;
+  return numbers.indexOf(bigger);
 }
 
 console.log(find_bigger([2, 3, 6, 7, 10, 1]));
+
+// 3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+
+function find_lower(numbers) {
+  lower = numbers[0];
+
+  for (number in numbers) {
+    if (numbers[number] < lower) {
+      lower = numbers[number];
+    }
+  }
+
+  return numbers.indexOf(lower);
+}
+
+console.log(find_lower([2, 4, 6, 7, 10, 0, -3]));
