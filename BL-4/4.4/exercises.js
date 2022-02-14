885da0448e7ed0192f028e65047b8a12e020c085
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-14 19:26:30 -0400

    add: ex 6 and 7

--- BL-4/4.4/exercises.js
+++ BL-4/4.4/exercises.js
@@ -67,4 +67,37 @@ function new_object() {
   }
 }
 
-new_object()
+
+// Usando o objeto abaixo, faça os exercícios a seguir:
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+/*
+6 - Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+*/
+
+function return_name_and_book() {
+  full_name = leitor.nome + ' ' + leitor.sobrenome;
+
+  console.log('O livro favorito de ' + full_name + ' se chama ' + leitor.livrosFavoritos[0].titulo);
+}
+
+// 7 - Adicione um novo livro favorito na chave livrosFavoritos , que é um array . Atribua a esta chave um objeto contendo as seguintes informações:
+
+new_book = {
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editor: 'Rocco',
+};
+
+leitor.livrosFavoritos.push(new_book);
