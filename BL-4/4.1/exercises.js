4a5b1434ebe47bcb6ab4d7e1bd560b9d65026b3a
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-14 11:30:02 -0400

    add: ex1

---
+++ BL-4/4.1/exercises.js
@@ -0,0 +1,33 @@
+// 1
+function sum(a, b) {
+  return a + b;
+}
+
+// 2
+function sub(a, b) {
+  return a - b;
+}
+
+// 3
+
+function mul(a, b) {
+  return a * b;
+}
+
+// 4
+function div(a, b) {
+  if (a <= 0) {
+    return "Não pode dividir por 0";
+  } else {
+    return a / b;
+  }
+}
+
+// 5
+function mod(a, b) {
+  if (a <= 0) {
+    return "Não pode dividir por 0";
+  } else {
+    return a % b;
+  }
+}
