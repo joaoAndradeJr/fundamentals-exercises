ee2aee88894e47bfb0d2374e734228f65b197a56
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-14 15:28:19 -0400

    add: ex 2 and 3

--- BL-4/4.3/exercises.js
+++ BL-4/4.3/exercises.js
@@ -12,4 +12,33 @@ function square(n) {
   }
 }
 
-square(5);
+// 2
+function triangle_retangle(n) {
+  let input = '';
+  let symbol = '*';
+
+  for (let i = 0; i < n; i += 1) {
+    input += symbol;
+    console.log(input);
+  }
+}
+
+// 3
+function inverted_triangle(n) {
+  let symbol = '*';
+  let inputLine = '';
+  let inputPosition = n;
+
+  for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+    for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+      if (columnIndex < inputPosition) {
+        inputLine += ' ';
+      } else {
+        inputLine += symbol;
+      }
+    }
+    console.log(inputLine);
+    inputLine = '';
+    inputPosition -= 1;
+  }
+}
