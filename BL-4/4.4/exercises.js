8be3b514e3c6482b55ac56f6aa1d6274546c51b2
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-14 19:28:15 -0400

    feat all exercises

--- BL-4/4.4/exercises.js
+++ BL-4/4.4/exercises.js
@@ -101,3 +101,7 @@ new_book = {
 };
 
 leitor.livrosFavoritos.push(new_book);
+
+// 8 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
+
+console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos');
