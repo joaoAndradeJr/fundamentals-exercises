e0f1bcc016e72c607a0dcf0c274a2c48431e2c9c
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-14 17:03:56 -0400

    add: ex 5

--- BL-4/4.4/exercises.js
+++ BL-4/4.4/exercises.js
@@ -46,3 +46,25 @@ function show_values() {
     console.log(info[values]);
   }
 }
+
+/*
+5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
+
+Margarida e Tio Patinhas
+Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178
+Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas
+Ambos recorrentes // Atenção para essa última linha!
+*/
+function new_object() {
+  let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+  };
+
+  for (value in info) {
+    console.log(info[value] + ' e ' + info2[value]);
+  }
+}
+
+new_object()
