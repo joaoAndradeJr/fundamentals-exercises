c94de0bc873a745f5c61637bc627791e487e461c
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-14 16:18:04 -0400

    feat: all exercises

--- BL-4/4.3/exercises.js
+++ BL-4/4.3/exercises.js
@@ -42,3 +42,27 @@ function inverted_triangle(n) {
     inputPosition -= 1;
   }
 }
+
+// 4
+function triangle(n) {
+  let symbol = '*';
+  let inputLine = '';
+
+  let midOfMatrix = (n + 1) / 2;
+  let controlLeft = midOfMatrix;
+  let controlRight = midOfMatrix;
+
+  for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
+    for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+      if (columnIndex > controlLeft && columnIndex < controlRight) {
+        inputLine = inputLine + symbol;
+      } else {
+        inputLine = inputLine + ' ';
+      }
+    }
+    console.log(inputLine);
+    inputLine = '';
+    controlRight += 1;
+    controlLeft -= 1
+  };
+}
