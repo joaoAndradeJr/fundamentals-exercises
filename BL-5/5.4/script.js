c8144ef5f7f5276f06c2d8c8bea9fe4af6154054
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-16 14:31:07 -0400

    add dinamic colors

--- BL-5/5.4/script.js
+++ BL-5/5.4/script.js
@@ -1,11 +1,41 @@
-let config = {
-  backgroundColor: 'black',
-  // backgroundColor: 'rgb(243, 243, 174)',
+const p = document.getElementsByClassName('square');
+
+for (let i = 0; i < p.length; i += 1) {
+  p[i].addEventListener('click', () => {
+    const attr = p[i].getAttribute('class');
+    const color = attr.substring(7);
+    const fontColor = getFontColor(color);
+    saveOnLocalStorage(color, fontColor);
+    changePageColor(color, fontColor);
+  });
 }
 
-localStorage.setItem('person', JSON.stringify(config));
+function changePageColor(bgColor, fontColor) {
+  const body = document.getElementsByTagName('body')[0];
+  body.setAttribute('class', bgColor);
+  body.style.color = fontColor;
+}
 
-let personal = JSON.parse(localStorage.getItem('person'));
+function getFontColor(color) {
+  switch (color) {
+  case 'black':
+    return 'white';
+  default:
+    return 'black';
+  }
+}
 
-document.getElementsByClassName('body')[0].style.backgroundColor = personal.backgroundColor;
-document.getElementsByClassName('body')[0].style.color = 'white';
+function saveOnLocalStorage(bgColor, color) {
+  const personal = {
+    backGround: bgColor,
+    fontColor: color, 
+  }
+
+  localStorage.setItem('person', JSON.stringify(personal));
+}
+
+
+// let personal = JSON.parse(localStorage.getItem('person'));
+
+// document.getElementsByClassName('body')[0].style.backgroundColor = personal.backgroundColor;
+// document.getElementsByClassName('body')[0].style.color = 'white';
