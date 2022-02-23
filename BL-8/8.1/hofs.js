3f2187907c20ce2c90956b3965f57da956732c6a
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-22 21:18:46 -0400

    add: generateEmail function

--- BL-8/8.1/hofs.js
+++ BL-8/8.1/hofs.js
@@ -1,8 +1,19 @@
-const newEmployees = () => {
+/*
+1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com .
+*/
+
+const generateEmail = (name) => {
+  const formatedName = name.replace(' ', '_').toLowerCase();
+  return `${formatedName}@trybe.com`;
+};
+
+const newEmployees = (generateEmail) => {
   const employees = {
-    id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: generateEmail('Pedro Guerra'),
+    id2: generateEmail('Luiza Drumond'),
+    id3: generateEmail('Carla Paiva'),
   }
   return employees;
 };
+
+console.log(newEmployees(generateEmail));
