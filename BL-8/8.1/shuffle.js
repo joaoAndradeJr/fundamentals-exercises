3169fbc015c88ac6bbe592b0a62fac11ded04250
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-22 21:40:41 -0400

    add: hof shuffleNumber

--- BL-8/8.1/shuffle.js
+++ BL-8/8.1/shuffle.js
@@ -1,5 +1,10 @@
-const shuffleNumber = () => (
-  parseInt(Math.random() * (6 - 1) + 1)
+const checkNumber = (number, luckNumber) => (
+  number === luckNumber ? 'Parabéns você ganhou' : 'Tente novamente'
 );
 
-console.log(shuffleNumber())
+const shuffleNumber = (number) => {
+  const luckNumber = parseInt(Math.random() * (6 - 1) + 1);
+  return checkNumber(number, luckNumber);
+};
+
+console.log(shuffleNumber(3))
