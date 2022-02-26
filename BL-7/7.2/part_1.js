8e9427bfe712a863706f0614200c77a7610c6bc5
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-25 21:03:06 -0400

    add: exercise part 1.2

--- BL-7/7.2/part_1.js
+++ BL-7/7.2/part_1.js
@@ -1,5 +1,5 @@
 const order = {
-  name: 'Rafael Andrade',
+  name: 'Luiz Silva',
   phoneNumber: '11-98763-1416',
   address: {
     street: 'Rua das Flores',
@@ -10,7 +10,7 @@ const order = {
     pizza: {
       marguerita: {
         amount: 1,
-        price: 25,
+        price: 20,
       },
       pepperoni: {
         amount: 1,
@@ -30,25 +30,24 @@ const order = {
     }
   },
   payment: {
-    total: 60,
+    total: 50,
   },
 };
 
-const customerInfo = (order) => {
-  return `Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, Nº ${order.address.number}, AP: ${order.address.apartment}.`;
-}
+const customerInfo = (order) => `Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, Nº ${order.address.number}, AP: ${order.address.apartment}.`;
 
-console.log(customerInfo(order));
+customerInfo(order);
 
-const orderModifier = (order) => {
-  // Adicione abaixo as informações necessárias.
+const orderModifier = (order) => `Olá ${order.name}, o total do seu pedido de ${Object.keys(order.order.pizza)} e ${order.order.drinks.coke.type} é R$ ${order.payment.total}.`
 
-}
-
-orderModifier(order);
+console.log(orderModifier(order));
 
 /*
-    Agora você vai fazer alguns exercícios de fixação.
+  1 - Agora você vai fazer alguns exercícios de fixação.
   Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
   Note que o parâmetro da função já está sendo passado na chamada da função.
-*/
+
+  2 - Complete a função orderModifier() para que seu retorno seja similar a "Olá Luiz Silva, o total do seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00."
+  Modifique o nome da pessoa compradora.
+  Modifique o valor total da compra para R$ 50,00.
+  */
