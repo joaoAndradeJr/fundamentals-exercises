7f772f45ae1d51ea975b5adfa417e46a23249e85
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-28 16:31:23 -0400

    add: bonus part 3

--- BL-8/8.1/bonus.js
+++ BL-8/8.1/bonus.js
@@ -46,3 +46,16 @@ const warriorDamage = () => {
   const maxValue = warrior.strength * warrior.weaponDmg;
   return getRandomNumber(warrior.strength, maxValue);
 };
+
+/*
+  Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
+  O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
+  A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de dano recebe uma mensagem (Ex: "Não possui mana suficiente") e a mana gasta é 0.
+*/
+
+const mageDamage = () => {
+  const maxValue = mage.intelligence * 2;
+  const damage = getRandomNumber(mage.intelligence, maxValue);
+  if (mage.mana < 15) return { damage: 'Não possui mana suficiente', mana: mage.mana }; 
+  return { damage: damage, mana: mage.mana - 15 };
+};
