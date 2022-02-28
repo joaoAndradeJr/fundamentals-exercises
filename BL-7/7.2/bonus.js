5b0c387a1054a066f78fce4482dc2f6106d3c5d6
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-28 15:18:36 -0400

    add: bonus 2

--- BL-7/7.2/bonus.js
+++ BL-7/7.2/bonus.js
@@ -17,3 +17,29 @@ const studentsAtMathLesson = () => {
 };
 
 console.log(studentsAtMathLesson());
+
+/*
+  Crie uma função que deverá retornar um objeto que representa o relatório do professor ou professora, as aulas que ele ou ela ministrou e o número total de estudantes. Use o objeto criado no exercício 5:
+*/
+
+const getInfo = (obj, name) => {
+  const allLessons = [];
+  let allStudent = 0;
+  const array = Object.values(obj);
+  for (index in array) {
+    if (array[index].professor === name) {
+      allLessons.push(array[index].materia)
+      allStudent += array[index].numeroEstudantes;
+    }
+  }
+  return { lessons: allLessons, estudantes: allStudent };
+}
+
+const createReport = (allLessons, name) => {
+  const report = {};
+  report.professor = name;
+  Object.assign(report, getInfo(allLessons, name));
+  return report;
+}
+
+console.log(createReport(assignObj(), 'Maria Clara')); 
